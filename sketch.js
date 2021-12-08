a80a261eee07199110c8635dd19dd67b7488ace3
Ishanmanesh <94181616+Ishanmanesh@users.noreply.github.com>
2021-12-08 13:47:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var path,pathImg;
+var runner,runnerAnimation;
+
+function preload() {
+   
+pathImg=loadImage("path.png");
+runnerAnimation=loadAnimation("Runner-1.png", "Runner-2.png");
+
+}
+
+function setup(){
+
+  path=createSprite(200,200)
+  path.addImage(pathImg)
+  path.velocityY=4;
+  path.scale=1.2;
+
+runner=createSprite(200,200)
+runner.addAnimation("runner",runnerAnimation);
+runner.scale=.1
+
+var invisGround = createSprite(50,100,9,4000)
+
+
+var invisGround2 = createSprite(399,80,10,4000)
+
+
+invisGround.visible=false
+invisGround2.visible=false
+
+
+
+
+
+}
+
+function draw() {
+  background(0)
+ 
+
+ 
+
+if(path.y>400){
+  path.y=height/2;
+}
+
+if(keyDown("left")){
+  runner.velocityX = -10;
+}
+
+if(keyDown("right")){
+  runner.velocityX=10;
+}
+
+
+
+
+
+  drawSprites();
+}
